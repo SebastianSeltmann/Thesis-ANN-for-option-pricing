924d08c2bee282f932d9058ed8e647acb9fa31b5
Sebastian Seltmann <s.git@rokomail.de>
2018-08-25 17:26:29 +0200

    reordered model definitions

--- models.py
+++ models.py
@@ -10,35 +10,6 @@ import tensorflow as tf
 
 from config import paths, onCluster
 
-def stupid_model():
-    model = Sequential()
-    model.add(Dense(12, input_dim=2, kernel_initializer='uniform', activation='relu'))
-    model.add(Dense(30,kernel_initializer='uniform',activation='relu'))
-    model.add(Dense(8,kernel_initializer='uniform',activation='relu'))
-    #model.add(Dense(1,kernel_initializer='uniform',activation='sigmoid'))
-    model.add(Dense(1, kernel_initializer='normal'))
-    #model.add(Dense(1,kernel_initializer='uniform',activation='softmax'))
-    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
-    model.save_weights(paths['weights'])
-
-    if not onCluster:
-        plot_model(model, to_file='model-diagrams/model-s.png')
-    return model
-
-
-def deep_model(layers=5, nodes_per_layer = 200, loss='mse', activation='relu'):
-    model = Sequential()
-    model.add(Dense(nodes_per_layer, input_dim=2, kernel_initializer='RandomNormal', activation=activation))
-    for i in range(layers-1):
-        model.add(Dense(nodes_per_layer, kernel_initializer='RandomNormal', activation=activation))
-    model.add(Dense(1, kernel_initializer='normal'))
-    model.compile(optimizer='rmsprop', loss=loss, metrics=['mae'])
-    model.save_weights(paths['weights'])
-    if not onCluster:
-        plot_model(model, to_file='model-diagrams/model-d.png')
-    return model
-
-
 def full_model(input_dim=2, num_layers=5, nodes_per_layer = 200, loss='mape', activation='relu', optimizer='adam',
                dropout_rate=0, use_batch_normalization=False, regularizer=None):
     model = Sequential()
@@ -63,6 +34,22 @@ def full_model(input_dim=2, num_layers=5, nodes_per_layer = 200, loss='mape', ac
         plot_model(model, to_file='model-diagrams/model-f.png')
     return model
 
+
+def black_scholes_price(m,t,r,s,type='call'):
+    d1 = 1/(s*(t**(1/2)))*(np.log(1/m)+(r+(s**2)/2)*t)
+    d2 = d1 - s*t**(1/2)
+    if type == 'put':
+        price = norm.cdf(d1) * 1 - norm.cdf(d2) * m * np.exp(-r * t)
+    elif type == 'call':
+        price = norm.cdf(-d2) * m * np.exp(-r * t) - norm.cdf(-d1) * 1
+    else:
+        raise ValueError("type must be 'call' or 'put'")
+    return price
+
+#-----------------------------------------------------------------------------
+# All models below this point are not used for the analysis in the thesis.
+# They are merely leftovers from the work-in-progress.
+#-----------------------------------------------------------------------------
 
 def multitask_loss(y_true, y_pred):
     return K.mean(K.sum(K.square(y_pred - y_true)), axis=-1)
@@ -98,21 +85,6 @@ def multitask_model(input_dim=2, shared_layers=5, individual_layers=3, nodes_per
         plot_model(model, to_file='model-diagrams/model-mt.png')
     model.compile(optimizer=optimizer, loss='mse', metrics=['mae'], loss_weights=[1., 1.])
     return model
-
-
-def adding_sample_model():
-    import keras
-
-    input1 = keras.layers.Input(shape=(16,))
-    x1 = keras.layers.Dense(8, activation='relu')(input1)
-    input2 = keras.layers.Input(shape=(32,))
-    x2 = keras.layers.Dense(8, activation='relu')(input2)
-    added = keras.layers.Add()([x1, x2])  # equivalent to added = keras.layers.add([x1, x2])
-
-    out = keras.layers.Dense(4)(added)
-    model = keras.models.Model(inputs=[input1, input2], outputs=out)
-    if not onCluster:
-        plot_model(model, to_file='model-diagrams/adding-sample.png')
 
 
 def rational_model(J=5):
@@ -232,7 +204,6 @@ def rational_multi_model(I=5, K=5, J=5):
         single_models.append(rational_model_v2(J=J, asLayer=True)(input))
 
     right = layers.Concatenate(name='single_models')(single_models)
-    # right = rational_model_v2(J=J, asLayer=True)(input)
 
     mult = layers.Multiply(name='combine')([left,right])
     out = layers.Lambda(lambda x: backend.sum(x, axis=1), output_shape=(1,), name='sum')(mult)
@@ -245,12 +216,55 @@ def rational_multi_model(I=5, K=5, J=5):
     return model
 
 
+def deep_model(layers=5, nodes_per_layer = 200, loss='mse', activation='relu'):
+    model = Sequential()
+    model.add(Dense(nodes_per_layer, input_dim=2, kernel_initializer='RandomNormal', activation=activation))
+    for i in range(layers-1):
+        model.add(Dense(nodes_per_layer, kernel_initializer='RandomNormal', activation=activation))
+    model.add(Dense(1, kernel_initializer='normal'))
+    model.compile(optimizer='rmsprop', loss=loss, metrics=['mae'])
+    model.save_weights(paths['weights'])
+    if not onCluster:
+        plot_model(model, to_file='model-diagrams/model-d.png')
+    return model
+
+
+def adding_sample_model():
+    import keras
+
+    input1 = keras.layers.Input(shape=(16,))
+    x1 = keras.layers.Dense(8, activation='relu')(input1)
+    input2 = keras.layers.Input(shape=(32,))
+    x2 = keras.layers.Dense(8, activation='relu')(input2)
+    added = keras.layers.Add()([x1, x2])  # equivalent to added = keras.layers.add([x1, x2])
+
+    out = keras.layers.Dense(4)(added)
+    model = keras.models.Model(inputs=[input1, input2], outputs=out)
+    if not onCluster:
+        plot_model(model, to_file='model-diagrams/adding-sample.png')
+
+
+def stupid_model():
+    model = Sequential()
+    model.add(Dense(12, input_dim=2, kernel_initializer='uniform', activation='relu'))
+    model.add(Dense(30,kernel_initializer='uniform',activation='relu'))
+    model.add(Dense(8,kernel_initializer='uniform',activation='relu'))
+    #model.add(Dense(1,kernel_initializer='uniform',activation='sigmoid'))
+    model.add(Dense(1, kernel_initializer='normal'))
+    #model.add(Dense(1,kernel_initializer='uniform',activation='softmax'))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    model.save_weights(paths['weights'])
+
+    if not onCluster:
+        plot_model(model, to_file='model-diagrams/model-s.png')
+    return model
+
+
 def custom_model(J=20, activation='relu', loss='mse'):
     options_input = layers.Input(shape=(2,), name='options_input')
     y = Dense(J, activation='softplus', name='softplus')(options_input)
     w = Dense(J, activation=activation, name=activation)(options_input)
     added = layers.Multiply()([y,w])
-    #step = layers.Dense(20, use_bias=False)(added)
     out = layers.Dense(1, use_bias=False)(added)
     model = models.Model(inputs=[options_input], outputs=out)
     model.save_weights(paths['weights'])
@@ -259,14 +273,3 @@ def custom_model(J=20, activation='relu', loss='mse'):
     model.compile(optimizer='rmsprop', loss=loss, metrics=['mae'])
     return model
 
-
-def black_scholes_price(m,t,r,s,type='call'):
-    d1 = 1/(s*(t**(1/2)))*(np.log(1/m)+(r+(s**2)/2)*t)
-    d2 = d1 - s*t**(1/2)
-    if type == 'put':
-        price = norm.cdf(d1) * 1 - norm.cdf(d2) * m * np.exp(-r * t)
-    elif type == 'call':
-        price = norm.cdf(-d2) * m * np.exp(-r * t) - norm.cdf(-d1) * 1
-    else:
-        raise ValueError("type must be 'call' or 'put'")
-    return price
